455070d4271bf03900c14d76a8f426dbc027177c
Brooks Van Buren <wvanbure@purdue.edu>
2017-03-12 21:15:59 -0400

    Difference between login issues fixed

--- public/staff/login.php
+++ public/staff/login.php
@@ -44,7 +44,7 @@ if(is_post_request() && request_is_same_domain()) {
       }
     } else {
       // No username found
-      $errors[] ="Log in was not successful.";
+      $errors[] ="Log in was unsuccessful.";
     }
   }
 }
